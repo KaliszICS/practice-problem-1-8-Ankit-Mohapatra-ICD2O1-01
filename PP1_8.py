c7152a73b002a002b3774e6a530f08670ed37397
Ankit-Mohapatra-ICD2O1-01 <Ankit.mohapatra@student.tdsb.on.ca>
2024-10-02 02:22:27 +0000

    Done

--- PP1_8.py
+++ PP1_8.py
@@ -6,26 +6,30 @@ def q1():
  print(bool1 or bool2)
 
 def q2():
-x = int(input("Enter an integer: "))
-print(x != 0)
+  x = int(input("Enter an integer: "))
+  print(x != 0)
 
 def q3():
   bool3 = int(input("Enter a number: "))
   print(bool3 > 0 and bool3 < 10)
 
 def q4():
- bool4 = input("Input food: ")
- bool5 = input("Input drink")
+  bool4 = input("Input food: ")
+  bool5 = input("Input drink: ")
+  if bool4 == "Pizza" or "pizza" and bool5 == "pop" or "Pop" :
+    print(True)
+  else :
+    print(False)
 
 
 def q5():
-  #Write Assignment code here
-
+  print(int(input("Enter an integer: ")) % 2 == 0)
+  
 #Do not edit code after this
 #Comment out the followwing code when running tests
 
-q1()
-q2()
-q3()
-q4()
-q5()
+#q1()
+#q2()
+#q3()
+#q4()
+#q5()
