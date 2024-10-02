45f160f561203b247ba662b2f38811bc007a751a
Ankit-Mohapatra-ICD2O1-01 <Ankit.mohapatra@student.tdsb.on.ca>
2024-10-02 02:37:05 +0000

    done again

--- PP1_8.py
+++ PP1_8.py
@@ -14,17 +14,15 @@ def q3():
   print(bool3 > 0 and bool3 < 10)
 
 def q4():
-  bool4 = input("Input food: ")
-  bool5 = input("Input drink: ")
-  if bool4 == "Pizza" or "pizza" and bool5 == "pop" or "Pop" :
-    print(True)
-  else :
-    print(False)
+  input("Input food: ")
+  input("Input drink: ")
+  print("pizza" == "pop")
 
 
 def q5():
-  print(int(input("Enter an integer: ")) % 2 == 0)
-  
+  x = int(input("Enter an integer: "))
+  print(f"The input {x} is {(x % 2 == 0)}.")
+
 #Do not edit code after this
 #Comment out the followwing code when running tests
 
