d1960b5ad90291972610875b748a157872f6ea68
Ankit-Mohapatra-ICD2O1-01 <Ankit.mohapatra@student.tdsb.on.ca>
2024-11-05 16:32:52 -0500

    Update PP1_8.py

--- PP1_8.py
+++ PP1_8.py
@@ -1,25 +1,25 @@
 
 def q1():
- bool1 = True
- bool2 = False
- print(bool1 and bool2)
- print(bool1 or bool2)
+   bool1 = True
+   bool2 = False
+   print(bool1 and bool2)
+   print(bool1 or bool2)
 
 def q2():
-  x = int(input("Enter an integer: "))
-  print(x != 0)
+   x = int(input("Enter an integer: "))
+   print(x != 0)
 
 def q3():
-  bool3 = int(input("Enter a number: "))
-  print(bool3 >= 0 and bool3 <= 10)
+   bool3 = int(input("Enter a number: "))
+   print(bool3 >= 0 and bool3 <= 10)
 
 def q4():
-x = input("Input food: ")
-y = input("Input drink: ")
-if x.lower() == "pizza" and y.lower() == "pop":
-    print(False)
-else:
-    print(True)
+ x = input("Input food: ")
+ y = input("Input drink: ")
+ if x.lower() == "pizza" and y.lower() == "pop":
+     print(False)
+ else:
+     print(True)
 
 
 def q5():
